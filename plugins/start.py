8fbfbd739ca8270d049eaf0867b0da65f411447b
Ayanokozii <ayanokozi0925@gmail.com>
2024-06-05 18:53:29 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -123,8 +123,14 @@ async def not_joined(client: Client, message: Message):
     buttons = [
        [
              InlineKeyboardButton("JOIN CHANNEL 1🎯", url=f"https://t.me/Dubbed_Hindi_Naruto_Shippudenn"),
-             InlineKeyboardButton("JOIN CHANNEL 2🎯", url=f"https://t.me/viru_writes")
+             InlineKeyboardButton("JOIN CHANNEL 2🎯", url=f"https://t.me/+fDCGP4BRtXNkYjE9")
         ],
+        [
+                InlineKeyboardButton(
+                    text = 'Try Again',
+                    url = f"https://t.me/{client.username}?start={message.command[1]}"
+                )
+            ]
     ]
     try:
         buttons.append(
